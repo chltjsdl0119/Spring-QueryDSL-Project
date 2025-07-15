e049d8e355e85ec1576036cb573f492e0a2bea3d
Seungwon-Choi <bestwondev@gmail.com>
2025-07-15 20:09:13 +0900

    initial commit

---
+++ src/main/java/com/springquerydsl/SpringQueryDslProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryDslProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQueryDslProjectApplication.class, args);
+    }
+
+}
